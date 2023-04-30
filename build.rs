ab6cde7f548fe625fe148348ff59dc49224b534a
Arrokoth <arrokoth233@gmail.com>
2023-04-30 19:31:53 +0200

    build.rs的小改

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
+#[cfg(target_os="windows")]
 extern crate windres;
 
 use windres::Build;
 
 fn main() {
     // https://learn.microsoft.com/zh-cn/windows/apps/design/style/iconography/app-icon-construction
+    #[cfg(target_os="windows")]
     Build::new().compile("assets/resources.rc").unwrap();
 }
