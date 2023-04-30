b490aa1f47fcd547c3b9706fc07c56d1ffbcb039
Arrokoth <arrokoth233@gmail.com>
2023-04-30 13:47:01 +0200

    好哎！我们的图标换力！

--- build.rs
+++ build.rs
@@ -3,5 +3,6 @@ extern crate windres;
 use windres::Build;
 
 fn main() {
+    // https://learn.microsoft.com/zh-cn/windows/apps/design/style/iconography/app-icon-construction
     Build::new().compile("assets/resources.rc").unwrap();
 }
