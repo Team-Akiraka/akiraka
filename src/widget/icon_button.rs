8fa569261130ad489144250a1b68a27401ef4b34
Arrokoth <arrokoth233@gmail.com>
2023-05-22 22:37:56 +0200

    你这按钮点击事件有点问题啊

--- src/widget/icon_button.rs
+++ src/widget/icon_button.rs
@@ -1,18 +1,16 @@
 use druid::{Affine, BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx, Point, Rect, RenderContext, Size, theme, UpdateCtx, Widget};
-use druid::widget::Image;
+use druid::widget::{Click, ControllerHost, Image};
 
 pub struct IconButton {
     image: Image,
-    size: Size,
-    pressing: bool
+    size: Size
 }
 
 impl IconButton {
     pub fn new(image: Image, size: Size) -> Self {
         Self {
             image,
-            size,
-            pressing: false
+            size
         }
     }
 }
@@ -20,25 +18,27 @@ impl IconButton {
 impl<T: Data> Widget<T> for IconButton {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         match event {
-            Event::MouseDown(m_event) => {
-                self.pressing = true;
-                // ctx.set_active(true);
-                ctx.request_paint();
+            Event::MouseDown(_) => {
+                if !ctx.is_disabled() {
+                    ctx.set_active(true);
+                    ctx.request_paint();
+                }
             }
-            Event::MouseUp(m_event) => {
-                self.pressing = false;
-                // ctx.set_active(false);
-                ctx.request_paint();
+            Event::MouseUp(_) => {
+                if ctx.is_active() && !ctx.is_disabled() {
+                    ctx.request_paint();
+                }
+                ctx.set_active(false);
             }
-            Event::MouseMove(m_event) => {
-                println!("{} : {}", ctx.is_active(), self.pressing);
-            }
-            _ => {}
+            _ => ()
         }
-        self.image.event(ctx, event, data, env);
+        // self.image.event(ctx, event, data, env);
     }
 
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
+        if let LifeCycle::HotChanged(_) | LifeCycle::DisabledChanged(_) = event {
+            ctx.request_paint();
+        }
         self.image.lifecycle(ctx, event, data, env);
     }
 
@@ -52,10 +52,10 @@ impl<T: Data> Widget<T> for IconButton {
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
-        let active = ctx.is_active();
+        let active = ctx.is_active() && !ctx.is_disabled();
         ctx.with_save(move |ctx| {
             let rect = Rect::from_origin_size(Point::ORIGIN, self.size);
-            ctx.fill(rect, &env.get(if self.pressing {
+            ctx.fill(rect, &env.get(if active {
                 theme::BACKGROUND_DARK
             } else {
                 theme::PLACEHOLDER_COLOR
