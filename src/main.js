4c2968616e68d62c48ad6c8b4a3dfe0a784e1494
Arrokoth <arrokoth233@gmail.com>
2023-06-29 20:27:32 +0200

    title bar

--- src/main.js
+++ src/main.js
@@ -11,8 +11,4 @@ async function greet() {
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
 });
