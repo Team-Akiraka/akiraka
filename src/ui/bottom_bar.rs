dd6c5eb8c41f8481c9c7dcaae21373a8d7da3c91
Arrokoth <arrokoth233@gmail.com>
2023-07-08 00:00:44 +0200

    Copied IconClearButton

--- src/ui/bottom_bar.rs
+++ src/ui/bottom_bar.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
-use druid::{Affine, BoxConstraints, Color, Data, Env, Event, EventCtx, Insets, LayoutCtx, LensExt, LifeCycle, LifeCycleCtx, PaintCtx, Point, RenderContext, Size, UpdateCtx, Vec2, WidgetExt, WidgetPod};
+use druid::{Affine, BoxConstraints, Color, Data, Env, Event, EventCtx, Insets, LayoutCtx, LensExt, LifeCycle, LifeCycleCtx, MouseButton, PaintCtx, Point, RenderContext, Size, UpdateCtx, Vec2, WidgetExt, WidgetPod};
 use druid::widget::{Widget, Flex, Radio, RadioGroup, Svg, SvgData, LineBreaking};
 use crate::{animations, Asset};
 use crate::theme::theme;
 use crate::ui::{download_page, hello_page, instances_page, settings_page};
-use crate::widget::icon_clear_button::IconClearButton;
+use crate::util::color_as_hex_string;
 use crate::widget::launch_button::LaunchButton;
 use crate::widget::profile_button::ProfileButton;
 
@@ -13,6 +13,101 @@ pub const BOTTOM_BAR_HEIGHT_NAV: f64 = 40.0;
 
 const ANIMATION_TIME: f64 = 0.5;
 static mut SELECTED: u64 = 0;
+
+const ICON_INSETS: Insets = Insets::uniform_xy(8., 2.);
+
+struct IconClearButton {
+    icon: Svg,
+    data: String,
+}
+
+impl IconClearButton {
+    pub fn new(data: String) -> IconClearButton {
+        Self {
+            icon: Svg::new(data.clone().replace("{color}", "#000000").parse::<SvgData>().unwrap()),
+            data
+        }
+    }
+}
+
+impl<T: Data> Widget<T> for IconClearButton {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, _data: &mut T, _env: &Env) {
+        match event {
+            Event::MouseDown(event) => {
+                if !ctx.is_disabled() && event.button == MouseButton::Left {
+                    ctx.set_active(true);
+                    ctx.request_paint();
+                }
+            }
+            Event::MouseUp(_) => {
+                if ctx.is_active() && !ctx.is_disabled() {
+                    ctx.request_paint();
+                }
+                ctx.set_active(false);
+            }
+            _ => (),
+        }
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
+        if let LifeCycle::HotChanged(_) | LifeCycle::DisabledChanged(_) = event {
+            ctx.request_paint();
+        }
+        self.icon.lifecycle(ctx, event, data, env)
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
+        self.icon.update(ctx, old_data, data, env)
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &T, env: &Env) -> Size {
+        let icon_size = self.icon.layout(ctx, bc, data, env);
+        let button_size =
+            bc.constrain(Size::new(
+                icon_size.width,
+                icon_size.height
+            ));
+        button_size
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
+        let is_active = ctx.is_active() && !ctx.is_disabled();
+        let is_hot = ctx.is_hot();
+        let size = ctx.size();
+        let stroke_width = env.get(druid::theme::BUTTON_BORDER_WIDTH);
+
+        let rounded_rect = size
+            .to_rect()
+            .inset(-stroke_width / 2.0)
+            .to_rounded_rect(env.get(druid::theme::BUTTON_BORDER_RADIUS));
+
+        let bg_gradient = if is_active {
+            env.get(theme::COLOR_CLEAR_BUTTON_ACTIVE)
+        } else if is_hot {
+            env.get(theme::COLOR_CLEAR_BUTTON_HOT)
+        } else {
+            env.get(theme::COLOR_CLEAR_BUTTON)
+        };
+
+        let border_color = if is_active {
+            env.get(theme::COLOR_CLEAR_BUTTON_BORDER_ACTIVE)
+        } else if is_hot {
+            env.get(theme::COLOR_CLEAR_BUTTON_BORDER_HOT)
+        } else {
+            env.get(theme::COLOR_CLEAR_BUTTON_BORDER)
+        };
+
+        ctx.fill(rounded_rect, &bg_gradient);
+
+        ctx.stroke(rounded_rect, &border_color, stroke_width);
+
+        ctx.with_save(|ctx| {
+            let svg_data = self.data.replace("{color}", color_as_hex_string(Color::from(env.get(crate::theme::theme::COLOR_TEXT))).as_str()).parse::<SvgData>().unwrap();
+            self.icon = Svg::new(svg_data);
+            self.icon.paint(ctx, data, env);
+        });
+    }
+}
 
 struct Child<T> {
     inner: WidgetPod<T, Box<dyn Widget<T>>>,
